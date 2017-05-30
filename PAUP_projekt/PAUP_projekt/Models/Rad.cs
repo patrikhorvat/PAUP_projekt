a7314b3eab3568a9b3eacb9512595550ba15900a
patrikhorvat <pakohorvat@gmail.com>
2017-05-30 12:49:19 +0200

    verzija_1
    osnovni kontroleri + modeli

---
+++ PAUP_projekt/PAUP_projekt/Models/Rad.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PAUP_projekt.Models
+{
+    public class Rad
+    {
+        public int ID { get; set; }
+
+        [DisplayName("Ime Rada")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [StringLength(160, ErrorMessage = "Ovo polje nesmije imati vise od 160 znakova")]
+        public string Ime { get; set; }
+
+        [DisplayName("File Rada")]
+        [StringLength(160, ErrorMessage = "Ovo polje nesmije imati vise od 160 znakova")]
+        public string Path { get; set; }
+
+        [DisplayName("Ime Korisnika")]
+        [StringLength(160, ErrorMessage = "Ovo polje nesmije imati vise od 160 znakova")]
+        public string Korisnik { get; set; }
+
+        [DisplayName("Zahtjev")]
+        public bool Zahtjev { get; set; }
+
+        [DisplayName("Kategorija")]
+        [ForeignKey("KategorijaIme")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        public int KategorijaID { get; set; }
+        public Kategorija KategorijaIme { get; set; }
+
+        [DisplayName("Struka")]
+        [ForeignKey("StrukaIme")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        public int StrukaID { get; set; }
+        public Struka StrukaIme { get; set; }
+    }
+}
