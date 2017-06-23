a3a993b01e86365f2fc46ed0a4d54df70bec4b32
patrikhorvat <pakohorvat@gmail.com>
2017-06-23 12:28:35 +0200

    v7

---
+++ PAUP_projekt/PAUP_projekt/Migrations/201705181151459_2.cs
@@ -0,0 +1,18 @@
+﻿namespace PAUP_projekt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class _2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Rads", "Zahtjev", c => c.Boolean(nullable: false));
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.Rads", "Zahtjev");
+        }
+    }
+}
